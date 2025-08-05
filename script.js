aa6768cfe02f787cdf5edb93bd0ad9df513a6939
marivi0606 <141031173+marivi0606@users.noreply.github.com>
2025-08-05 12:19:15 -0300

    Update script.js

--- script.js
+++ script.js
@@ -19,54 +19,54 @@ const perguntas = [
         ]
     },
     {
-        enunciado: "Com a descoberta desta tecnologia, chamada Inteligência Artificial, uma professora de tecnologia da escola decidiu fazer uma sequência de aulas sobre esta tecnologia. No fim de uma aula ela pede que você escreva um trabalho sobre o uso de IA em sala de aula. Qual atitude você toma?",
+        enunciado: "Após aceitar a missão, o caçador de tumbas te deu um mapa antigo. No entanto, as coordenadas parecem estar escritas em uma linguagem desconhecida. Você tem que decifrá-las para encontrar o caminho para as tumbas. Qual atitude você toma?",                                                
         alternativas: [
             {
-                texto: "Utiliza uma ferramenta de busca na internet que utiliza IA para que ela ajude a encontrar informações relevantes para o trabalho e explique numa linguagem que facilite o entendimento.",
-                afirmacao: "Conseguiu utilizar a IA para buscar informações úteis."
+                texto: "Você utiliza uma ferramenta de tradução online para decifrar as coordenadas. A ferramenta, por sorte, tem um banco de dados de línguas antigas e consegue fazer a tradução.",
+                afirmacao: "Você consegue traduzir as coordenadas e traçar uma rota segura para as tumbas, evitando as dunas mais perigosas."
             },
             {
-                texto: "Escreve o trabalho com base nas conversas que teve com colegas, algumas pesquisas na internet e conhecimentos próprios sobre o tema.",
-                afirmacao: "Sentiu mais facilidade em utilizar seus próprios recursos para escrever seu trabalho."
+                texto: "Você estuda os símbolos e os compara com livros de história e arqueologia que trouxe consigo, usando seu conhecimento para encontrar um padrão e decifrar as coordenadas.",
+                afirmacao: "Você sente mais confiança em decifrar as coordenadas com base em seu próprio conhecimento e esforço, e a tradução se prova precisa."
             }
         ]
     },
     {
-        enunciado: "Após a elaboração do trabalho escrito, a professora realizou um debate entre a turma para entender como foi realizada a pesquisa e escrita. Nessa conversa também foi levantado um ponto muito importante: como a IA impacta o trabalho do futuro. Nesse debate, como você se posiciona?",
+        enunciado: "No caminho para o túmulo, vocês encontram um acampamento abandonado. A equipe de arqueólogos que estava ali desapareceu, mas deixou para trás um diário. A última entrada fala sobre uma maldição que protegeria o túmulo e um mapa que levaria a uma recompensa ainda maior. Como você se posiciona?",
         alternativas: [
             {
-                texto: "Defende a ideia de que a IA pode criar novas oportunidades de emprego e melhorar habilidades humanas.",
-                afirmacao: "Vem impulsionando a inovação na área de IA e luta para abrir novos caminhos profissionais com IA."
+                texto: "Você defende a ideia de que a maldição é apenas uma lenda e que o mapa pode levar a uma descoberta valiosa que pode ser usada para o bem da humanidade.",
+                afirmacao: "Você inspira a equipe a continuar, usando a sua fé na ciência para seguir o mapa, e acaba descobrindo um novo túmulo que contém um tesouro ainda maior."
             },
             {
-                texto: "Me preocupo com as pessoas que perderão seus empregos para máquinas e defendem a importância de proteger os trabalhadores.",
-                afirmacao: "Sua preocupação com as pessoas motivou a criar um grupo de estudos entre trabalhadores para discutir meios de utilização de IA de forma ética."
+                texto: "Você se preocupa com o destino dos arqueólogos desaparecidos e defende a ideia de voltar e pedir ajuda, pois uma maldição é algo muito sério e pode ser perigoso demais para a equipe continuar.",
+                afirmacao: "Sua preocupação com a segurança da equipe motiva você a alertar a equipe de resgate local. A equipe é salva, mas a recompensa menor e o caçador de tumbas não está muito feliz."
             }
         ]
     },
     {
-        enunciado: "Ao final da discussão, você precisou criar uma imagem no computador que representasse o que pensa sobre IA. E agora?",
+        enunciado: "Ao chegar na entrada do túmulo, vocês notam um hieróglifo estranho, que ninguém da equipe sabe o que significa. A recompensa está logo atrás do hieróglifo. O que você faz?",
         alternativas: [
             {
-                texto: "Criar uma imagem utilizando uma plataforma de design como o Paint.",
-                afirmacao: "Notou também que muitas pessoas não sabem ainda utilizar as ferramentas tradicionais e decidiu compartilhar seus conhecimentos de design utilizando ferramentas de pintura digital para iniciantes."
+                texto: "Você tenta desenhar o hieróglifo com os materiais que tem, depois tira uma foto e envia para um especialista, que em tempo real te diz o que significa.",
+                afirmacao: "A mensagem do especialista foi clara e a equipe conseguiu decifrar o hieróglifo a tempo, abrindo a porta para a recompensa. Você também se ofereceu para ajudar outros aventureiros em situações similares."
             },
             {
-                texto: "Criar uma imagem utilizando um gerador de imagem de IA.",
-                afirmacao: "Acelerou o processo de criação de trabalhos utilizando geradores de imagem e agora consegue ensinar pessoas que sentem dificuldades em desenhar manualmente como utilizar também!"
+                texto: "Você tenta entender o que o hieróglifo significa e como ele se encaixa na cultura local.",
+                afirmacao: "A sua insistência em entender a cultura local e a história do túmulo te ajudam a encontrar a passagem secreta para a câmara do tesouro, onde a recompensa está guardada."
             }
         ]
     },
     {
-        enunciado: "Você tem um trabalho em grupo de biologia para entregar na semana seguinte, o andamento do trabalho está um pouco atrasado e uma pessoa do seu grupo decidiu fazer com ajuda da IA. O problema é que o trabalho está totalmente igual ao do chat. O que você faz? ",
+        enunciado: "Você e a equipe conseguem chegar ao tesouro, que é um baú cheio de moedas e artefatos. No entanto, o caçador de tumbas tenta roubar tudo. O que você faz?",
         alternativas: [
             {
-                texto: "Escrever comandos para o chat é uma forma de contribuir com o trabalho, por isso não é um problema utilizar o texto inteiro.",
-                afirmacao: "Infelizmente passou a utilizar a IA para fazer todas suas tarefas e agora se sente dependente da IA para tudo."
+                texto: "Você luta contra o caçador de tumbas e o vence, pegando todo o tesouro para você e sua equipe.",
+                afirmacao: "Você e sua equipe saem do deserto com o tesouro, mas com a polícia na cola. O caçador de tumbas, antes de ser preso, consegue escapar e jura vingança. Você, agora, está com a sua vida em risco."
             },
             {
-                texto: "O chat pode ser uma tecnologia muito avançada, mas é preciso manter a atenção pois toda máquina erra, por isso revisar o trabalho e contribuir com as perspectivas pessoais é essencial.",
-                afirmacao: "Percebeu que toda IA reproduz orientações baseadas na empresa que programou e muito do que o chat escrevia não refletia o que pensava e por isso sabe que os textos gerados pela IA devem servir como auxílio e não resultado final. "
+                texto: "Você tenta negociar com o caçador de tumbas, oferecendo uma parte do tesouro para ele ir embora e deixar o restante para você e sua equipe.",
+                afirmacao: "O caçador de tumbas aceita a oferta, mas antes de ir embora, avisa que o baú é amaldiçoado. Vocês conseguem sair do deserto, mas a maldição traz má sorte para a vida de todos os envolvidos. "
             }
         ]
     },
